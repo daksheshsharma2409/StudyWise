60be4330d7e5fb0cacd5d1afe87906e38832a94f
Dakshesh Sharma <daksheshsharma2007@gmail.com>
2026-09-05 01:27:36 +0530

    Frontend & Document Opening Support

--- backend/server.js
+++ backend/server.js
@@ -11,18 +11,15 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/resources", resourceRoutes);
-app.use("/api/subjects", subjectRoutes);
 app.use(
     cors({
-        origin: [
-            "http://localhost:3000",
-            "https://study-wise-lake.vercel.app/",
-        ],
+        origin: ["http://localhost:3000", "https://study-wise-lake.vercel.app"],
         credentials: true,
     }),
 );
 
+app.use("/api/resources", resourceRoutes);
+app.use("/api/subjects", subjectRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
